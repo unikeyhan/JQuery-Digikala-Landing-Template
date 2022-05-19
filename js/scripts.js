cbcf06b9d6b6822feda4b60f0a7e48c883790d8c
Alireza Shahryari <eng.alirezashahryari@gmail.com>
2022-05-20 00:23:02 +0430

    Final sample

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+new WOW().init();
+
+$(".owl-carousel").owlCarousel({
+    rtl:true,
+    loop:true,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+});
